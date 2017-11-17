082d14615f526fbc3f73669f0cf42f422d10647f
Rik van Beek <2021698@talnet.nl>
2017-11-17 03:17:37 +0100

    second commit

--- main.php
+++ main.php
@@ -1,2 +1,15 @@
 <?php
-    echo 'Hello world!';
+	
+	$leeftijd = 65;
+
+	if ($leeftijd < 3) {
+		echo "0 euro";
+	} else if ($leeftijd <= 12) {
+		echo "5 euro";
+	} else if ($leeftijd > 65) {
+		echo "5 euro";
+	} else {
+		echo "10 euro";
+	}
+
+    echo "<br>leeftijd: ".$leeftijd;
